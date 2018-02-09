2d876507199a6c36bb5a7c8ac9efda395fe96775
LeonardoBanegas99 <leobanegas99@unitec.edu>
2018-02-09 15:02:20 -0600

    login

--- Lab-3_Leonardo_Samuel/src/lab/pkg3_leonardo_samuel/Lab3_Leonardo_Samuel.java
+++ Lab-3_Leonardo_Samuel/src/lab/pkg3_leonardo_samuel/Lab3_Leonardo_Samuel.java
@@ -1,12 +1,51 @@
 package lab.pkg3_leonardo_samuel;
 
+import java.util.Scanner;
+
 public class Lab3_Leonardo_Samuel {
+
+    private Scanner sc = new Scanner(System.in);
+    private ConnectiveLine Cline;
 
     public static void main(String[] args) {
         System.out.println("m");
         System.out.println("a");
-        
-        
+
     }
-    
+
+    private void logIn() {
+        System.out.println("Ingrese su correo: ");
+        String correo = sc.next();
+        System.out.println("Ingrese su contraseña: ");
+        String contra = sc.next();
+        for (int i = 0; i < Cline.getEmpresas().size(); i++) {
+            if ((Cline.getEmpresas().get(i).getCorreo().equals(correo)) && (Cline.getEmpresas().get(i).getContra().equals(contra))) {
+                logInEmpresas();
+            }
+        }
+        for (int i = 0; i < Cline.getPersonas().size(); i++) {
+            if ((Cline.getPersonas().get(i).getCorreo().equals(correo))) {
+                if (((Administrador) Cline.getPersonas().get(i)).getContrasena().equals(contra)) {
+                    logInAdministrador();
+                }
+                if (((Freelance) Cline.getPersonas().get(i)).getContrasena().equals(contra)) {
+                    logInFreelance();
+                }
+            }
+        }
+
+    }
+
+    private void logInEmpresas() {
+
+    }
+
+    private void logInAdministrador() {
+
+    }
+
+    private void logInFreelance() {
+
+    }
+
 }
